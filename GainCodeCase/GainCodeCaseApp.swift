45cee2c8a490b0ef4a2157724d5049067466ef8b
egeseckin <ege.seckin@outlook.com>
2021-11-12 10:34:46 +0300

    Initial Commit

---
+++ GainCodeCase/GainCodeCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GainCodeCaseApp.swift
+//  GainCodeCase
+//
+//  Created by Ege Seçkin on 12.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct GainCodeCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
